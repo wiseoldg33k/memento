c35b0621e60fc26a4ebad6074a633ef82a486e8d
Wise Old G33k <wiseoldg33k@gmail.com>
2019-02-27 21:49:45 +0100

    use existing salt for password hashing

--- memento/__init__.py
+++ memento/__init__.py
@@ -1,13 +1,17 @@
 import bcrypt
+import base64
+import hashlib
+
 
 class State:
-
-    GENSALT_ROUNDS = 14
+    def __init__(self, salt):
+        self.salt = salt
 
     def db_should_be_created(self):
         return not self.db_exists
 
     def hash_pin(self, pin):
-        return bcrypt.hashpw(
-                     pin.encode('utf-8'), 
-                     bcrypt.gensalt(rounds=self.GENSALT_ROUNDS))
+        fixed_size_password = base64.b64encode(
+            hashlib.sha256(pin.encode("utf-8")).digest()
+        )
+        return bcrypt.hashpw(fixed_size_password, self.salt)
--- tests/test_login.py
+++ tests/test_login.py
@@ -2,15 +2,17 @@ import pytest
 import time
 from memento import State
 
+
 @pytest.fixture
-def slow_state():
-    state = State()
+def state():
+    salt = b"$2b$04$d7JNTkkjB3vLqmMMoX/Mzu"
+    state = State(salt=salt)
     return state
 
 @pytest.fixture
-def state(slow_state):
-    slow_state.GENSALT_ROUNDS = 1
-    return slow_state
+def slow_state(state):
+    state.salt = b'$2b$14$BMOTE4FHkpRb1tU1heKnFe'
+    return state
 
 
 def test_it_creates_a_database_if_it_does_not_exist(state):
@@ -22,13 +24,14 @@ def test_it_does_not_create_a_database_if_it_exists(state):
     state.db_exists = True
     assert not state.db_should_be_created()
 
+
 @pytest.mark.slow
-@pytest.mark.parametrize('pincode', ['12345', '00000', 'éric'])
+@pytest.mark.parametrize("pincode", ["12345", "00000", "éric"])
 def test_pin_code_hashing_takes_more_than_a_second(slow_state, pincode):
 
     start = time.perf_counter()
 
-    slow_state.hash_pin(pincode)
+    hashed = slow_state.hash_pin(pincode)
 
     end = time.perf_counter()
 
