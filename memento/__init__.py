1bfbfacc130679d0de79ad7cfab56281067b1af4
Wise Old G33k <wiseoldg33k@gmail.com>
2019-02-27 21:15:48 +0100

    added hashing speed tests

--- memento/__init__.py
+++ memento/__init__.py
@@ -1,4 +1,13 @@
+import bcrypt
+
 class State:
+
+    GENSALT_ROUNDS = 14
 
     def db_should_be_created(self):
         return not self.db_exists
+
+    def hash_pin(self, pin):
+        return bcrypt.hashpw(
+                     pin.encode('utf-8'), 
+                     bcrypt.gensalt(rounds=self.GENSALT_ROUNDS))
--- tests/test_login.py
+++ tests/test_login.py
@@ -1,13 +1,37 @@
+import pytest
+import time
 from memento import State
 
-
-def test_it_creates_a_database_if_it_does_not_exist():
+@pytest.fixture
+def slow_state():
     state = State()
+    return state
+
+@pytest.fixture
+def state(slow_state):
+    slow_state.GENSALT_ROUNDS = 1
+    return slow_state
+
+
+def test_it_creates_a_database_if_it_does_not_exist(state):
     state.db_exists = False
     assert state.db_should_be_created()
 
 
-def test_it_does_not_create_a_database_if_it_exists():
-    state = State()
+def test_it_does_not_create_a_database_if_it_exists(state):
     state.db_exists = True
     assert not state.db_should_be_created()
+
+@pytest.mark.slow
+@pytest.mark.parametrize('pincode', ['12345', '00000', 'éric'])
+def test_it_creates_a_database_with_an_original_pin_code(slow_state, pincode):
+
+    start = time.perf_counter()
+
+    slow_state.hash_pin(pincode)
+
+    end = time.perf_counter()
+
+    elapsed = end - start
+
+    assert elapsed > 1
